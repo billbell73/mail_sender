87a2d6f2bdcdf071f3ad1c981c3a82ef490b4b8a
Will Bell <willbell@gmail.com>
2014-02-12 15:32:10 +0000

    Tidied up

--- mail_sender_script.rb
+++ mail_sender_script.rb
@@ -24,12 +24,12 @@ def send_email(row)
     from 'Your Name <name@domain.com>'
     subject 'Time capsule!!'
     text_part do
-      body "Here is your message from the past: #{row["message"]}
-              Click link to see video: #{row["video_url"]}"
+      body "Hello"     
     end
     html_part do
       content_type 'text/html; charset=UTF-8'
-      body "<b>Your message from the past: #{row["message"]}</b>"
+      body "<p>Your message from the past: #{row["message"]}</p>" +
+            "<p>Click link to see video: #{row["video_url"]}</p>"
     end
   end
 end
@@ -37,7 +37,12 @@ end
 
 uri = URI.parse(URI.encode(ENV['DATABASE_URL']))
 
-conn = PG.connect(:dbname => uri.path[1..-1], :user => uri.user, :password => uri.password, :host => uri.host, :port => uri.port)
+conn = PG.connect(:dbname => uri.path[1..-1], 
+                  :user => uri.user, 
+                  :password => uri.password, 
+                  :host => uri.host, 
+                  :port => uri.port)
+
 res = conn.exec("SELECT * FROM timecapsules")
 res.each do |row| 
 	date_past = Date.parse(row["send_at"]) < Date.today
